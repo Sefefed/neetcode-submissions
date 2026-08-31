80bcdd4d6d81a4c54240dbe1656c9c2a8fc1c6ab
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-08-31 22:43:00 +0300

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def climb(cur):
+            if cur in memo:
+                return memo[cur]
+            if cur == n:
+                return 1
+            if cur > n:
+                return 0
+            memo[cur] = climb(cur + 1) + climb(cur + 2)  
+            return memo[cur]          
+        return climb(0)    
+
+            
+            
+                    
+        
