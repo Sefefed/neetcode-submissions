716cffaf9958e106ae23544ce8b7eab26ac28aae
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-09-02 15:38:14 +0300

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            mid = i + (j - i) // 2
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                i = mid + 1   
+            else:
+                j = mid - 1
+        return i        
+
+        
+
+
+
+            
