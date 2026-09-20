86c21fa91eb5cded5156844f399e6822b52da25d
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-09-20 23:28:37 +0300

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.py
@@ -0,0 +1,32 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+    def put(self, key: int, value: int) -> None:
+        index = key % self.size
+
+        for i, (k, v) in enumerate(self.buckets[index]):
+            if k == key:
+                self.buckets[index][i] = (key, value)
+                return
+
+        self.buckets[index].append((key, value))
+
+    def get(self, key: int) -> int:
+        index = key % self.size
+
+        for k, v in self.buckets[index]:
+            if k == key:
+                return v
+
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = key % self.size
+
+        for i, (k, v) in enumerate(self.buckets[index]):
+            if k == key:
+                self.buckets[index].pop(i)
+                return
